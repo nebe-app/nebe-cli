8caab8f75802fff0653116fb7f410303532bccab
Hynek Zatloukal <hynek.zatloukal@isobar.com>
2020-07-21 10:09:47 +0200

    Commands

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,28 @@
 #!/usr/bin/env node
 
+const _ = require('lodash');
+const fs = require('fs');
+const chalk = require('chalk');
+const checkForUpdate = require('../lib/utils/checkForUpdate.js');
+
 const [, , ...args] = process.argv;
 
-console.log(args);
+let commandName = 'help';
+
+if (args.length > 0) {
+	commandName = _.toString(args[0]);
+}
+
+if (!fs.existsSync(`${__dirname}/../lib/commands/${commandName}.js`)) {
+	console.error(`Command`, chalk.red(commandName), `does not exist. Run`, chalk.blue(`nebe help`), `for the command list`);
+	process.exit(0);
+}
+
+checkForUpdate();
+
+(async () => {
+	const CommandClass = require(`../lib/commands/${commandName}.js`);
+	const command = new CommandClass();
+	await command.handle();
+	process.exit(0);
+})();
