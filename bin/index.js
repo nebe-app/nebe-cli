08a812646b8b0f07f9a73ea76e94b5dba337a8dd
Hynek Zatloukal <hynek.zatloukal@isobar.com>
2020-07-24 09:53:20 +0200

    Wip git

--- bin/index.js
+++ bin/index.js
@@ -5,6 +5,8 @@ const fs = require('fs');
 const chalk = require('chalk');
 const checkForUpdate = require('../lib/utils/checkForUpdate.js');
 
+checkForUpdate();
+
 const [, , ...args] = process.argv;
 
 let commandName = 'help';
@@ -17,8 +19,6 @@ if (!fs.existsSync(`${__dirname}/../lib/commands/${commandName}.js`)) {
 	console.error(`Command`, chalk.red(commandName), `does not exist. Run`, chalk.blue(`nebe help`), `for the command list`);
 	process.exit(0);
 }
-
-checkForUpdate();
 
 (async () => {
 	const CommandClass = require(`../lib/commands/${commandName}.js`);
